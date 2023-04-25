f6948739b406ac312c08cda4c8b77d808fe746c0
MalikNegmatov <maliknegmatov@gmail.com>
2023-04-25 09:41:15 +0300

    Добавили решение задачи 2

--- src/Main.java
+++ src/Main.java
@@ -6,26 +6,31 @@ public class Main {
         int[] index = new int[] {1, 2, 3};
         double[] vector =  {1.57, 7.654, 9.986};
         double[] tensor = {1, 0, 0, 0, 1, 0, 0, 0, 0.001};
+        System.out.println("Задача №1 \nМассивы index, vector и tensor инициализированы.");
 
-        task2(index);
-        task3(vector, 3);
-        task3(tensor, 4);
+        task2(index, vector, tensor);
     }
 
 
-    public static void task2(int[] array) {
-        System.out.println("Задача №2");
+    public static void task2(int[] arrayIndex, double[] arrayVector, double[] arrayTensor) {
+        System.out.println("\nЗадача №2");
         // Пишем код для задачи 2
-        String strOutput = Integer.toString(array[0]);
-        for (int i = 1; i < array.length; i++) strOutput = strOutput + ", " + array[i];
-        System.out.println(strOutput+ "\n");
+        String strOutput = Integer.toString(arrayIndex[0]);
+        for (int i = 1; i < arrayIndex.length; i++) strOutput = strOutput + ", " + arrayIndex[i];
+        System.out.println(strOutput);
+
+        strOutput = Double.toString(arrayVector[0]);
+        for (double j : arrayVector) strOutput = strOutput + ", " + j;
+        System.out.println(strOutput);
+
+        strOutput = Double.toString(arrayTensor[0]);
+        for (double j : arrayTensor) strOutput = strOutput + ", " + j;
+        System.out.println(strOutput);
     }
 
     public static void task3(double[] array, int taskNumber) {
         System.out.println("Задача №" + taskNumber);
         // Пишем код для задач 3-4
-        String strOutput = Double.toString(array[0]);
-        for (double j : array) strOutput = strOutput + ", " + j;
-        System.out.println(strOutput+ "\n");
+
     }
 }
